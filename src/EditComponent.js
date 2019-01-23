f7f1dc3ffa5fbd6245fe6778aa093b0d235c8d17
Ramon <ralmatob@gmail.com>
2019-01-23 18:21:00 +0100

    Styling editbox

--- src/EditComponent.js
+++ src/EditComponent.js
@@ -6,12 +6,11 @@ export default class EditComponent extends Component{
     constructor(props){
         super(props);
         
-        let fact = props.factor;
-        let finf = props.finfo;
+        let factor, finfo, visible = this.props;
         this.state = {
-            factor: fact||"A",
-            finfo: finf||{name:props.factor,low:-1,high:1},
-            visible: this.props.visible,
+            factor: factor||"Default",
+            finfo: finfo||{name:props.factor,low:-1,high:1},
+            visible,
         };
     }
     _onSaveHandler = () => {
@@ -26,50 +25,63 @@ export default class EditComponent extends Component{
             this.props.onCancel();
         }
     }
+    static getDerivedStateFromProps(nextProps, prevState){
+        console.log("Calling getDerivedStateFromProps");
+        if(nextProps.factor!==prevState.factor){
+            return {
+                factor: nextProps.factor,
+                finfo: nextProps.finfo,
+            };
+        }else{
+            return null;
+        }
+    }
     render(){
         return(
-            <div className="editbox_container"visible={this.state.visible}>
-                <div className="editbox_text">
-                    Factor Name
-                </div>
-                <input 
-                    type="text" 
-                    onChange={(e) => {
-                        let nobj = this.state.finfo;
-                        nobj.name = e.target.value;
-                        this.setState({finfo:nobj});
-                    }} 
-                    value={this.state.finfo.name}/>
-                <div className="edit_levels_container">
-                <div>
+            <div className="editbox_container">
+                <div className="editbox_content">
                     <div className="editbox_text">
-                        Low
+                        Factor Name
                     </div>
-                    <input
-                        type="text"
+                    <input 
+                        type="text" 
                         onChange={(e) => {
                             let nobj = this.state.finfo;
-                            nobj.low = e.target.value;
-                            this.setState({finfo: nobj});
-                        }}
-                        value={this.state.finfo.low} />
-                </div>
-                <div>
-                    <div className="editbox_text">
-                        High
+                            nobj.name = e.target.value;
+                            this.setState({finfo:nobj});
+                        }} 
+                        value={this.state.finfo?this.state.finfo.name:"Default"}/>
+                    <div className="edit_levels_container">
+                    <div>
+                        <div className="editbox_text">
+                            Low
+                        </div>
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                let nobj = this.state.finfo;
+                                nobj.low = e.target.value;
+                                this.setState({finfo: nobj});
+                            }}
+                            value={this.state.finfo?this.state.finfo.low:-1} />
                     </div>
-                    <input
-                        type="text"
-                        onChange={(e) => {
-                            let nobj = this.state.finfo;
-                            nobj.high = e.target.value;
-                            this.setState({finfo: nobj});
-                        }}
-                        value={this.state.finfo.high} />
+                    <div>
+                        <div className="editbox_text">
+                            High
+                        </div>
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                let nobj = this.state.finfo;
+                                nobj.high = e.target.value;
+                                this.setState({finfo: nobj});
+                            }}
+                            value={this.state.finfo?this.state.finfo.high:1} />
+                    </div>
+                    </div>
+                    <ButtonComponent value="Cancel" onClick={this._onCancelHandler.bind(this)} />
+                    <ButtonComponent value="Save" onClick={this._onSaveHandler.bind(this)} />
                 </div>
-                </div>
-                <ButtonComponent value="Cancel" onClick={this._onCancelHandler.bind(this)} />
-                <ButtonComponent value="Save" onClick={this._onSaveHandler.bind(this)} />
             </div>
         );
     }
