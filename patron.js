797f4d8cd5f7904ab34f3e0395ddfaf023bd68c5
Ramon <ralmatob@gmail.com>
2019-02-01 17:58:57 +0100

    minor changes

--- patron.js
+++ patron.js
@@ -14,8 +14,25 @@ class Patron{
             let a = g.split("=");
             let b = a[1];
             a = a[0];
+            //a=b
+            //I=ab
         }
     }
 }
+const simplify = (s) => {
+    let ns = s.toUpperCase();
+    ns = ns.split("").sort().join("");
+    let fs = "";
+    for(let i=0;i<ns.length-1;i+=1){
+        let a = ns[i],b = ns[i+1];
+        if(a===b){
+            fs += "I";
+        }else{
+            fs += a+b;
+        }
+    }
+    console.log(fs);
+}
 
-var p = new Patron(["D=AB","E=AC"]);
+// var p = new Patron(["D=AB","E=AC"]);
+simplify("abdsbade");
