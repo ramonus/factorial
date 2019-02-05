1271b9fff6efab222873d588b1622dcd7731c91f
Ramon <ralmatob@gmail.com>
2019-02-05 13:32:23 +0100

    Improves

--- patron.js
+++ patron.js
@@ -10,6 +10,14 @@ class Patron{
             this.input_generators = [gen.toUpperCase()];
         }
         this.relation_definition = this._def_rel();
+        this.depending_factors = this._get_depending_factors();
+    }
+    _get_depending_factors(){
+        let depending = [];
+        for(let i=0;i<this.input_generators.length;i++){
+            depending.push(this.input_generators[i].split("=")[0]);
+        }
+        return depending;
     }
     relation_definition_str(){
         return ["I",...this.relation_definition].join("=");
@@ -43,6 +51,18 @@ class Patron{
         }
         return max_factor;
     }
+    _combinations(str){
+        let combinations = [];
+        for(let i=0;i<str.length;i++){
+            for(let j=i;j<str.length;j++){
+                let el = simplify(str[i]+str[j]);
+                if(el!="I"){
+                    combinations.push(el);
+                }
+            }
+        }
+        return combinations;
+    }
     resolution(){
         return Math.min(...this.relation_definition.map(it=>it.length));
     }
@@ -52,18 +72,40 @@ class Patron{
         let curr_factor = "A";
         while(words.indexOf(curr_factor)<=words.indexOf(max_factor)){
             let confusions = this._confusions(curr_factor);
-            if(confusions){
+            let ci = this.depending_factors.indexOf(confusions[0]);
+            if(confusions!=null&ci==-1){
                 main_confusions.push(confusions);
             }
             curr_factor = words[words.indexOf(curr_factor)+1];
         }
         return main_confusions;
     }
+
+    _two_factor_confusions(){
+        let tfconfusions = [];
+        const str = words.slice(0,Math.min(...this.depending_factors.map(e=>words.indexOf(e))));
+        const combinations = this._combinations(str);
+        for(let i=0;i<combinations.length;i++){
+            let confusions = this._confusions(combinations[i]);
+
+            tfconfusions.push(confusions);
+        }
+        return tfconfusions;
+    }
+    _contains_factors(str,facts){
+        for(let i=0;i<facts.length;i++){
+            let letter = facts[i];
+            if(str.indexOf(letter)>-1){
+                return true;
+            }
+        }
+        return false;
+    }
     _confusions(facts){
         let confusions = [simplify(facts)];
         for(let i=0;i<this.relation_definition.length;i++){
             let confu = simplify(facts+this.relation_definition[i]);
-            if(confu.indexOf(facts)==-1){
+            if(!this._contains_factors(confu,facts)){
                 confusions.push(confu);
             }
         }
@@ -123,6 +165,9 @@ console.log("Relació de definició:","\n\t"+p.relation_definition_str());
 console.log("Resolució:",p.resolution());
 console.log("Confusions:");
 p._main_confusions().forEach(it => {
+    console.log("\t"+it.join("+"));
+});
+p._two_factor_confusions().forEach(it => {
     console.log("\t"+it.join("+"));
 });
 // simplify("abdsbade");
